ef69eea6db6687e7634a36ad9b7ed3fa8f6a7f69
Imagine <imagine10255@gmail.com>
2015-10-15 08:44:08 +0800

    Update start.sh
    debug cp /opt/backup/mysql-data/* $DIRECTORY

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ DIRECTORY="/usr/local/mariadb/var/"
 # Check DB Files
 if [ "`ls -A $DIRECTORY`" = "" ]; then
   echo "$DIRECTORY not database files, run copy default database files to here"
-  cp /opt/backup/mysql-data/* $DIRECTORY
+  cp -r /opt/backup/mysql-data/* $DIRECTORY
 fi
 
 
